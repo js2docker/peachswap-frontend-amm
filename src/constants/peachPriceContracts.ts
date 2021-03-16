b089252db54ca87eda9041432c64d2f14447ca1e
JuicyPeachSwapDefi <80694311+JuicyPeachSwapDefi@users.noreply.github.com>
2021-03-16 19:16:20 +0100

    Update and rename eggPriceContracts.ts to peachPriceContracts.ts

---
+++ src/constants/peachPriceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: { cakeAddress: string; busdAddress: string; lpAddress: string } = {
+  cakeAddress: '0x9469B619f991a63b59C333f5ffc86FD3011Ce044', //  testnet address - to be updated
+  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+  lpAddress: '0xDEa2006B9AB89b1a4fc3f82d09E230B74583fD3d', //  testnet address - to be updated
+}
+
+export default priceContracts
