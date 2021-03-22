0430ad6676b2dcb9a5de0c02a644c4a84772dace
JuicyPeachSwapDefi <80694311+JuicyPeachSwapDefi@users.noreply.github.com>
2021-03-22 14:35:41 +0100

    Update peachPriceContracts.ts

--- src/constants/peachPriceContracts.ts
+++ src/constants/peachPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: { cakeAddress: string; busdAddress: string; lpAddress: string } = {
   cakeAddress: '0x9512e7fD29c187B31b2405396EC4217624EEC08c', //  testnet address - to be updated
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56', // testnet address - to be updated
-  lpAddress: '0x2Fa3e558c5927eDA44C2F940AD8A40fe47191B89', //  testnet address - to be updated
+  lpAddress: '0x8c2ae236ca6c638ac9cad9a5ad29b297758c7182', //  testnet address - to be updated
 }
 
 export default priceContracts
